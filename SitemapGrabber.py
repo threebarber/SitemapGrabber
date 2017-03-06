c7b3da228f6b001da4fd2bef020557507b4b8852
threebarber <jblack25@students.towson.edu>
2017-03-05 19:19:32 -0500

    Add files via upload

---
+++ SitemapGrabber.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from pws import Google
+from pws import Bing
+from config import *
+from google import search
+
+#don't touch any of this just edit the config file 
+
+sitemapfile = open(filename,'w+')
+
+try:
+    for url in search('inurl:\"/sitemap_products_1.xml\" intext:'+keyword,lang='es', stop=numresults):
+        print("[+]Retrieved Sitemap Containing Keyword \"" +keyword+"\": "+url)
+        if "?" in url:
+            url = url.split("?")[0]
+        else:
+            pass
+        sitemapfile.write(url+"\n")
+except Exception, e:
+    print str(e)+"[-]Google is probably blocking you so wait and try later or something"
+
+
+    #yes this is literally like 10 lines of code 
---
+++ config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+filename          = 'sitemaplinklist.txt' #filename to save sitemap links to
+numresults        = 30  #number of results to retrieve 
+keyword           = "adidas consortium" #keyword to search for within sitemap
+
+#note: if you put the number of results to like 1000 ur gonna get blocked (temporarily)
+#probably gonna get blocked if you spam it too so yea
+#fixes to allow u kids to spam this as much as you want coming later hopefully
